425af9d403978454f75a5e240f3c642aa1cc70bb
silviapalves <silviapalves@yahoo.com.br>
2021-05-16 12:48:21 -0300

    Update index.php

--- Deploy/index.php
+++ Deploy/index.php
@@ -3,6 +3,6 @@
   <title>Teste PHP</title>
  </head>
  <body>
- <?php echo "<p>Ei Natacha, mals a demora!</p>"; ?>
+ <?php echo "<p>Ei Itu! Queria te dar parabens pela sua formatura mesmo que com um certo atraso, mas do fundo da minha alma (eu nao quiz fazer isso antes num momento que eu tava era te pedindo ajuda). Esse momento é especial dmais, afinal você formou em HARVARD, quanto sucesso meu deus. Queria aproveitar também pra dar parabens por simplesmente voce ser quem vc é e te agradecer por tanto, serio, vc é uma das pessoas que eu mais admiro na vida, pelo esforço, pela sua natureza, pela sua simplicidade, pela sua humanidade em especial!! Enfim, torço sempre sempre por voce e quero poder continuar acompanhando de perto tudo que vc ainda tem pra oferecer pra esse mundão! Saudades de voce e mais uma vez parabens por ser tão gigante! Esse foi meu primeiro site da vida e achei digno usar ele pra te mandar essa mensagem!!!</p>"; ?>
  </body>
 </html>
